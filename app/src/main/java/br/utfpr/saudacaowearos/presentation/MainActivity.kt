6e10e1da8c5767984e1675af1681bb1cdaca9052
Matheus Toledo <matheus.silva256@fatec.sp.gpv.br>
2025-10-18 13:56:42 -0300

    Initial commit

---
+++ app/src/main/java/br/utfpr/saudacaowearos/presentation/MainActivity.kt
@@ -0,0 +1,69 @@
+/* While this template provides a good starting point for using Wear Compose, you can always
+ * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter to find the
+ * most up to date changes to the libraries and their usages.
+ */
+
+package br.utfpr.saudacaowearos.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.TimeText
+import androidx.wear.tooling.preview.devices.WearDevices
+import br.utfpr.saudacaowearos.R
+import br.utfpr.saudacaowearos.presentation.theme.SaudacaoWearOSTheme
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+
+        setTheme(android.R.style.Theme_DeviceDefault)
+
+        setContent {
+            WearApp()
+        }
+    }
+}
+
+@Composable
+fun WearApp() {
+
+    val horas = SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date())
+
+    Box(
+        modifier = Modifier.fillMaxSize().padding(8.dp), 
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Olá, Wear OS 6, Hora atual é $horas",
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    WearApp()
+}
